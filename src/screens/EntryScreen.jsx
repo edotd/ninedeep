15cb764bb36f901747df1f31988ea17e4d3a3268
edotd <148269796+edotd@users.noreply.github.com>
2026-09-23 22:39:41 -0700

    Correct start screen version label

--- src/screens/EntryScreen.jsx
+++ src/screens/EntryScreen.jsx
@@ -87,7 +87,7 @@ export default function EntryScreen({ pendingJoinCode, joinOnly = false, soloSta
 
           {!joinOnly && tab === 'solo' && (
             <>
-              <div className="entry-version">v.10</div>
+              <div className="entry-version">v1.0</div>
               <div className="entry-field-group">
                 <div className="entry-field-heading">
                   <span>Franchise Name</span>
