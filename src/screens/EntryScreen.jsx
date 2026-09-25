f9b5401122b2caf94be2c717e5fba54f354bd98f
edotd <148269796+edotd@users.noreply.github.com>
2026-09-25 11:48:34 -0700

    Bump version to v1.03

--- src/screens/EntryScreen.jsx
+++ src/screens/EntryScreen.jsx
@@ -87,7 +87,7 @@ export default function EntryScreen({ pendingJoinCode, joinOnly = false, soloSta
 
           {!joinOnly && tab === 'solo' && (
             <>
-              <div className="entry-version">v1.02</div>
+              <div className="entry-version">v1.03</div>
               <div className="entry-field-group">
                 <div className="entry-field-heading">
                   <span>Franchise Name</span>
